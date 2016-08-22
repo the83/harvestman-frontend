9f980b7c027dfefcb84c5681ec8e3102878ede30
Theron Humiston <theronhumiston@gmail.com>
2016-08-21 22:31:31 -0500

    post and homepage styling improvements

--- app/index/route.js
+++ app/index/route.js
@@ -1,14 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  session: Ember.inject.service('session'),
   model: function() {
     var posts = this.store.query('post', {});
-    var products = this.store.query('product', {});
     return Ember.RSVP.hash({
       posts: posts,
-      isIndex: true,
-      isAuthenticated: this.get('session.isAuthenticated')
     });
   }
 });
--- app/post/model.js
+++ app/post/model.js
@@ -4,5 +4,8 @@ export default DS.Model.extend({
   permalink: DS.attr('string'),
   title: DS.attr('string'),
   content: DS.attr('string'),
-  images: DS.hasMany('post/image', { async: 'false' })
+  images: DS.hasMany('post/image', { async: 'false' }),
+  heroImage: Ember.computed('images.[]', function() {
+    return this.get('images.firstObject.image.url');
+  })
 });
