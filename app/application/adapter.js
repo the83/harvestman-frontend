0a9ec5edb67557131c6ecae823d4392ce6901e24
Theron Humiston <theronhumiston@gmail.com>
2016-09-03 17:00:59 -0500

    generic ajax error handler

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -6,4 +6,18 @@ export default ActiveModelAdapter.extend({
   authorizer: 'authorizer:devise',
   host: config.backendHost,
   namespace: 'api',
+  flashMessages: Ember.inject.service(),
+  ajax(url, type, hash) {
+    let promise = this._super(url, type, hash);
+    promise.then(() => {
+      if (type === 'POST') {
+        this.get('flashMessages').success('Created!', { sticky: true });
+      } else if (type === 'PUT') {
+        this.get('flashMessages').success('Updated!', { sticky: true });
+      }
+    }).catch((error) => {
+      this.get('flashMessages').danger(error, { sticky: true });
+    });
+    return promise;
+  }
 });
