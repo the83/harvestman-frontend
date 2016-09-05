7df38c38bee909c5239707274f74c76b0f8c4491
Theron Humiston <theronhumiston@gmail.com>
2016-09-05 18:55:29 -0500

    don't display image carousel if product has one image

--- app/components/hm-product-display/component.js
+++ app/components/hm-product-display/component.js
@@ -5,5 +5,9 @@ export default Ember.Component.extend({
   showDescription: Ember.computed.equal('selectedTab', 'description'),
   showFeatures: Ember.computed.equal('selectedTab', 'features'),
   showManual: Ember.computed.equal('selectedTab', 'manual'),
-  showWhereToBuy: Ember.computed.equal('selectedTab', 'whereToBuy')
+  showWhereToBuy: Ember.computed.equal('selectedTab', 'whereToBuy'),
+  showSlideshow: Ember.computed.gt('product.images.length', 1),
+  firstImageUrl: Ember.computed('product.images.[]', function() {
+    return this.get('product.images.firstObject.image.url');
+  })
 });
