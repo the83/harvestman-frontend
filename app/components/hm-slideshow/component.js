1c1c6cd764ddd6a3f14da9e454c2dbabc4a58099
Theron Humiston <theronhumiston@gmail.com>
2016-09-05 18:39:25 -0500

    fix product carousel

--- app/components/hm-slideshow/component.js
+++ app/components/hm-slideshow/component.js
@@ -1,11 +1,26 @@
 import Ember from 'ember';
+const CAROUSEL_INTERVAL = 5000;
 
 export default Ember.Component.extend({
   didInsertElement() {
     $('.carousel-item').eq(0).addClass('active');
+    const carousel = $('.carousel').carousel({
+      interval: CAROUSEL_INTERVAL
+    });
+    this.carousel = carousel;
   },
 
   elementId: Ember.computed('slideshowId', function() {
     return '#' + this.get('slideshowId');
-  })
+  }),
+
+  actions: {
+    goPrev() {
+      this.carousel.carousel('prev');
+    },
+
+    goNext() {
+      this.carousel.carousel('next');
+    }
+  }
 });
