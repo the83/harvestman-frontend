7172e512633e15d53249ae25c2e611442f861df9
Theron Humiston <theronhumiston@gmail.com>
2016-12-17 18:37:38 -0600

    featured products sidebar

--- app/helpers/file-name.js
+++ app/helpers/file-name.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function fileName(params/*, hash*/) {
   const model = params[0];
   const type = params[1];
-  return model.get('name') || model.get(type).get('url');
+  return model.get('name') || model.get(type)['url'];
 }
 
 export default Ember.Helper.helper(fileName);
--- app/index/route.js
+++ app/index/route.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var posts = this.store.query('post', {});
+    const posts = this.store.query('post', {});
+    const products = this.store.query('product', {});
+
     return Ember.RSVP.hash({
       posts: posts,
+      products: products,
     });
   }
 });
