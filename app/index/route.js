fb49f5fc7a1e880f51958da4b9afdf7a1aadea85
Theron Humiston <theronhumiston@gmail.com>
2017-07-01 15:38:30 -0500

    pass featured flag on homepage products

--- app/index/route.js
+++ app/index/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     const posts = this.store.query('post', {});
-    const products = this.store.query('product', {});
+    const products = this.store.query('product', { featured: true });
 
     return Ember.RSVP.hash({
       posts: posts,
